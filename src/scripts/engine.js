71de336b0b0de9d5d070aeeec70f6e381486b4cd
raizarochaw <raizarocha8@gmail.com>
2024-02-18 17:17:14 -0300

    feat: Cria os states da engine

---
+++ src/scripts/engine.js
@@ -0,0 +1,24 @@
+// objeto de objetos, onde será guardado o estado de memória das propriedades/características
+const state = {
+  score: {
+    playerScore: 0,
+    computerScore: 0,
+    scoreBox: document.getElementById("score_points"),
+  },
+  cardSprites: {
+    avatar: document.getElementById("card-image"),
+    avatar: document.getElementById("card-name"),
+    avatar: document.getElementById("card-type"),
+  },
+  fieldCards: {
+    player: document.getElementById("player-field-card"),
+    player: document.getElementById("computer-field-card"),
+  },
+  actions: {
+    button: document.getElementById("next-duel"),
+  }
+};
+
+function init() {};
+
+init();
